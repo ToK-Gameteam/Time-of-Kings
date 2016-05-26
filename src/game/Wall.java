f72df4f92640f82492463ae11b3461eaf652a56d
Cromewell <jrlacherman@googlemail.com>
2016-05-26 13:30:16 +0200

    Update Wall.java

--- src/game/Wall.java
+++ src/game/Wall.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.awt.Point;
+
 /**
  * 
  */
@@ -8,6 +10,6 @@ public class Wall extends Building {
 	private static final long serialVersionUID = 1L;
 
 	public Wall(int number, int locationX, int locationY){
-		super(10, 10, 10, number, locationX, locationY);
+		super(10, 10, 10, number, new java.awt.Point(locationX, locationY));
 	}
 }
