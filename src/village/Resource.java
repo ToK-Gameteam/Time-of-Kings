7897a5f7f480e631feeff78a2a6b2b581c7de02c
SupressWarnings <constantinschulte@web.de>
2016-06-19 20:40:40 +0200

    new package and improvements
    Improving all sourcecode

---
+++ src/village/Resource.java
@@ -0,0 +1,60 @@
+package game.village;
+
+import java.io.Serializable;
+
+/**
+ * Class Resource
+ * 
+ * The class Resource creates a special type of resource (wood, stone, iron), which has a value and a limit.
+ * It implements the functions to add or subtract a value, and for admins only, to set the value of the resource.
+ * There also setter and getter for the limit.
+ * 
+ * @author Constantin Schulte
+ * @version 0.2
+ **/
+public class Resource implements Serializable {
+	static final long serialVersionUID = 1;
+	
+	private final String type;
+	private int value;
+	private int limit;
+	
+	public Resource( String type, int limit ){
+		this.type = type;
+		this.limit = limit;
+		value = 100;
+	}
+	
+	public String getType(){
+		return type;
+	}
+	
+	public int getValue(){
+		return value;
+	}
+	
+	public int getLimit(){
+		return limit;
+	}
+	
+	public void setValue( int value ){
+		if( value > limit){
+			value = limit;
+		}
+		this.value = value;
+	}
+	
+	public void addValue( int value ){
+		this.value += value;
+		if( this.value > limit ){
+			this.value = limit;
+		}
+	}
+	
+	public void subtractValue( int value ){
+		this.value -= value;
+		if( this.value < 0 ) {
+			this.value = 0;
+		}
+	}
+}
