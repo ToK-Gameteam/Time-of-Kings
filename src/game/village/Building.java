3090df4a0c48953a23655c2525d01ecab50fa3c5
SupressWarnings <constantinschulte@web.de>
2016-06-19 20:31:37 +0200

    New package and improved sourcecode
    Improved sourcecode

---
+++ src/game/village/Building.java
@@ -0,0 +1,69 @@
+package game.village;
+
+import java.io.Serializable;
+import game.*;
+
+/**
+- * Class Building
+- * 
+- * The class Building creates a building, which has a level (at the beginning 1), a number (depending on the number of buildings of this type build before),
+- * a location saved in a java.awt.Point, and allows to level the building up and to change the location.
+- * 
+- * @author Constantin Schulte
+- * @version 0.0
+- **/
+public abstract class Building implements Serializable, BuildingUpgrade {
+	static final long serialVersionUID = 1;
+	
+	protected String type;
+	protected Location location;
+	protected int upgradeCost;
+	protected int level;
+	protected int hitpoints;
+	
+	public Building( int upgradeCost, Location location, int hitpoints, String type ){
+		this.type = type;
+		this.upgradeCost = upgradeCost;
+		this.location = location;
+		this.hitpoints = hitpoints;
+		level = 1;
+	}
+	
+	public String getType(){
+		return type;
+	}
+	
+	public int getLevel(){
+		return level;
+	}
+	
+	public int getUpgradeCosts(){
+		return upgradeCost;
+	}
+	
+	public int getHitpoints(){
+		return hitpoints;
+	}
+	
+	public Location getLocation(){
+		return location;
+	}
+	
+	public void setLocation( Location location ){
+		this.location = location;
+	}
+	
+	public int[] levelUp(int[] resources){
+		if( resources[0] >= upgradeCost && resources[1] >= upgradeCost && resources[3] >= upgradeCost && level < 7 ){
+			++level;
+			upgradeSpecification();
+			int[] costs = { upgradeCost, upgradeCost, upgradeCost };
+			return costs;
+		}else{
+			int[] nullCost = { 0, 0, 0 };
+			return nullCost;
+		}
+	}
+	
+	@Override public void upgradeSpecification(){}
+}
