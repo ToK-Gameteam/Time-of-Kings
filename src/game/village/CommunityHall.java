1de8841c06ed1d0884f811e68cf2f2aa61cd1778
SupressWarnings <constantinschulte@web.de>
2016-07-05 14:18:42 +0200

    Update and new package
    updated and moved to new package.

---
+++ src/game/village/CommunityHall.java
@@ -0,0 +1,22 @@
+package game.village;
+
+import game.Location;
+
+public class CommunityHall extends Building {
+	static final long serialVersionUID = 1;
+	
+	private int capacity;
+	
+	public CommunityHall( Location location ){
+		super( 10, location, 100, "Lager" );
+		capacity = 300;
+	}
+	
+	public int getCapacity(){
+		return capacity;
+	}
+	
+	@Override public void upgradeSpecification(){
+		hitpoints *= 2;
+	}
+}
