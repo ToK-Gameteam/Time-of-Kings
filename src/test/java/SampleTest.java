550491402d9fd083d715cf8aa2d306c2f3c6ed9b
Jan Scheidegger <scheideggerj@gmail.com>
2017-03-27 19:29:06 +0200

    added sample test

--- src/test/java/SampleTest.java
+++
@@ -1,7 +0,0 @@
-package test;
-
-/**
- * Created by jan on 27.03.17.
- */
-public class SampleTest {
-}
---
+++ src/test/java/de/tok_gameteam/tok/MainTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by jan on 27.03.17.
+ */
+public class MainTest {
+
+}
